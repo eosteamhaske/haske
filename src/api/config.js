b5ae376f7c881c8630ac3cc75d87ac59e8484281
eltNEG <yorewole@gmail.com>
2018-10-17 04:44:01 +0100

    Update Api

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,5 @@
 export default {
   verbose: false,
-  chainId: "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca",
-  keyProvider: ["5KJ91a7yNS2xtio1HPeLSm34xF8tu4BPVdtEeYqDSHCj9STrGNj"],
-  httpEndpoint: "http://dev.cryptolions.io:38888"
+  keyProvider: ["5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3", "5JaFcbo7w27nTCULsmVz5dJvH7yXqqFiDLksouXHhidbDkahrJo"],
+  httpEndpoint: "http://192.168.99.100:8888"
 };
--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,12 @@ import Eos from "eosjs";
 import config from "./config";
 
 const eos = Eos(config);
-console.log(eos.getInfo());
+eos.getInfo({}).then(console.log);
 eos.getBlock("3000").then(console.log);
+eos
+  .getCurrencyBalance("eosio.token", "eosteamhaske")
+  .then(tokenBalance => console.log(tokenBalance));
+/*
 const options = {
   authorization: "eosteamhaske@active",
   user: "eosteamhaske",
@@ -14,7 +18,33 @@ eos
   .contract("haskehelloc1")
   .then(contract => contract.hi("eosteamhaske", options))
   .then(console.log);
-
+*/
+const transfer = () => {
+  eos
+  .transaction({
+    actions: [
+      {
+        account: "eosio.token",
+        name: "transfer",
+        authorization: [
+          {
+            actor: "eosteamhaske",
+            permission: "active"
+          }
+        ],
+        data: {
+          from: "eosteamhaske",
+          to: "user1",
+          quantity: `2.0000 TOK`,
+          memo: "some description of the transaction"
+        }
+      }
+    ]
+  })
+  .then(result => {
+    console.log('done')
+  });
+}
 const api = "api";
 
 export default api;
