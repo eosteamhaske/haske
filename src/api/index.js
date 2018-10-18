2fd85ecc1cfee2883c52016757ee14b586dbf75f
eltNEG <yorewole@gmail.com>
2018-10-18 03:33:32 +0100

    update api

--- src/api/index.js
+++ src/api/index.js
@@ -2,19 +2,53 @@ import Eos from "eosjs";
 import config from "./config";
 
 const eos = Eos(config);
-console.log(eos.getInfo());
-eos.getBlock("3000").then(console.log);
-const options = {
-  authorization: "eosteamhaske@active",
-  user: "eosteamhaske",
-  broadcast: true,
-  sign: true
-};
-eos
-  .contract("haskehelloc1")
-  .then(contract => contract.hi("eosteamhaske", options))
-  .then(console.log);
 
-const api = "api";
+const getInfo = () => eos.getInfo().then(info => info);
+
+const getBlock = blockNumber =>
+  eos.getBlock(blockNumber).then(blockInfo => blockInfo);
+
+//"eosio.token" , "eosteamhaske"
+const getBalance = (tokenName, accountName) =>
+  eos
+    .getCurrencyBalance(tokenName, accountName)
+    .then(tokenBalance => tokenBalance);
+
+// "eosio.token", "eosteamhaske", "eosteamhaske", "user1", `2.0000 TOK`, memo
+const transfer = (account, actor, from, to, quantity, memo = "memo") =>
+  eos
+    .transaction({
+      actions: [
+        {
+          account: account,
+          name: "transfer",
+          authorization: [
+            {
+              actor: actor,
+              permission: "active"
+            }
+          ],
+          data: {
+            from: from,
+            to: to,
+            quantity: quantity,
+            memo: memo
+          }
+        }
+      ]
+    })
+    .then(result => result);
+
+//const registerUser
+//const userHistory
+//const applyLoan
+//const payBackLoan
+
+const api = {
+  getInfo,
+  getBlock,
+  getBalance,
+  transfer
+};
 
 export default api;
