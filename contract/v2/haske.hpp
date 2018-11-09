38699523eac117c4ee34418e9f1bd9aa103cfab8
Shalom Nyende <shalom.nyende@gmail.com>
2018-11-09 08:38:11 +0300

    Init V2 - Header definition + basic implementation

--- contract/v2/haske.hpp
+++ contract/v2/haske.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-
+// Avoid "using namsepace <>" in headers. Be explicit here
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
 #include <eosiolib/action.hpp>
@@ -22,36 +22,37 @@ class [[eosio::contract]] haske : public eosio::contract {
 
 
         [[eosio::action]]
-        void register(eosio::name account, std::string first_name, std::string last_name, std::string country, std::string mobile, std::string biometrics );
+        void reguser(eosio::name uesername, std::string first_name, std::string last_name, std::string country, std::string mobile, std::string biometrics);
 
         [[eosio::action]]
-        void placecollateral(eosio::name borrower, std::string collateral_name, std::string collateral_description, std::string ipfs_collateral_url, std::vector<eosio::name> guarantors);
+        void placecollat(eosio::name borrower, std::string collateral_name, std::string collateral_description, std::string ipfs_collateral_url, std::vector<eosio::name> guarantors);
 
         [[eosio::action]]
         void guarantee(eosio::name guarantor, uint64_t collateral_id, uint64_t level_od_confidence);
 
         [[eosio::action]]
-        void placelendable(eosio::name lender, asset amount, std::string terms);
+        void placelendable(eosio::name lender, eosio::asset amount, std::string terms);
 
-        // TODO
-        [[eosio::action0]]
-        void fund();
+        [[eosio::action]]
+        void fund(eosio::name lender, eosio::name borrower, eosio::asset amount, uint64_t lendable_id);
+
     private:
+
       struct [[eosio::table("users")]] user {
         uint64_t id;
-        eosio::name account;
+        eosio::name uesername;
         std::string first_name;
         std::string last_name;
         std::string country;
         std::string mobile;
         std::string biometrics;
 
-        auto primary_key() const { return account.value; }
+        auto primary_key() const { return uesername.value; }
         uint64_t by_id() const { return id; }
       } ;
 
       typedef eosio::multi_index<"users"_n, user, 
-              eosio::indexed_by<"account"_n, eosio::const_mem_fun<user, uint64_t, &user::by_id>>> users_table;
+              eosio::indexed_by<"id"_n, eosio::const_mem_fun<user, uint64_t, &user::by_id>>> users_table;
 
     struct [[eosio::table("collaterals")]] collateral {
         uint64_t id;
@@ -61,8 +62,21 @@ class [[eosio::contract]] haske : public eosio::contract {
         std::string ipfs_path;
         std::vector<eosio::name> guarantors;
         bool is_guaranteed;
+
+        auto primary_key() const { return id; }
+        uint64_t by_borrower() const { return user.value; }
+    };
+
+    typedef eosio::multi_index<"collaterals"_n, collateral,
+            eosio::indexed_by<"users"_n, eosio::const_mem_fun<collateral, uint64_t, &collateral::by_borrower>>> collaterals_table;
+
+
+    struct [[eosio::table("lendables")]] lendable {
+        uint64_t id;
+        // TODO
     };
 };
+
 
 
 
